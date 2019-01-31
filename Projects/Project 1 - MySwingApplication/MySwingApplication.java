78611763932ccb0e78c2bd05e5bb765c2d528ec7
793724 <42591733+793724@users.noreply.github.com>
2019-01-31 10:35:58 -0800

    Started project 1

---
+++ Projects/Project 1 - MySwingApplication/MySwingApplication.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+/**
+ * Project 1 - MySwingApplication
+ *
+ * @ Emma chiu
+ * @ 1/31/19
+ */
+
+public class MySwingApplication implements Runnable {
+    public void run() {
+    }
+    
+    public static void main() {
+    }
+}

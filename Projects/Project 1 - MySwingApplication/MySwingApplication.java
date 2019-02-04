04e89ff1f7af2d60fef857966a57037dce88c23e
793724 <42591733+793724@users.noreply.github.com>
2019-02-04 09:43:15 -0800

    Activity 1 complete, MySwingApplication

--- Projects/Project 1 - MySwingApplication/MySwingApplication.java
+++ Projects/Project 1 - MySwingApplication/MySwingApplication.java
@@ -2,14 +2,21 @@ import javax.swing.*;
 /**
  * Project 1 - MySwingApplication
  *
- * @ Emma chiu
+ * @ Emma Chiu
  * @ 1/31/19
  */
 
 public class MySwingApplication implements Runnable {
+    JFrame jFrame;
     public void run() {
+        jFrame = new JFrame("Window");
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
     }
     
     public static void main() {
+       MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+       javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
     }
 }
